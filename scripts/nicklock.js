cea27115f5548d5f4e004b7a05ab57753cefe0f3
johnkentm6-afk <johnkentm6@gmail.com>
2025-12-14 11:49:36 +0800

    Update nicklock.js

--- scripts/nicklock.js
+++ scripts/nicklock.js
@@ -1,57 +1,40 @@
-let nickLockStatus = {};
-let savedNicknames = {};
+global.nickLock = global.nickLock || {};
+global.savedNick = global.savedNick || {};
 
 module.exports = {
   config: {
     name: "nicklock",
-    version: "1.0",
+    version: "1.2",
     author: "ChatGPT",
     role: 1,
-    description: "Lock nicknames of all users",
+    description: "Lock all nicknames in group",
     category: "admin",
-    usages: "&nicklock on\n&nicklock off",
+    usages: "&nicklock on | &nicklock off",
     cooldowns: 5
   },
 
-  onStart: async function ({ api, event, args }) {
+  run: async function ({ api, event, args }) {
     const threadID = event.threadID;
 
+    if (!args[0])
+      return api.sendMessage("&nicklock on | &nicklock off", threadID);
+
     if (args[0] === "off") {
-      nickLockStatus[threadID] = false;
-      return api.sendMessage("🔓 Nickname Lock OFF", threadID);
+      global.nickLock[threadID] = false;
+      return api.sendMessage("🔓 Nickname lock OFF", threadID);
     }
 
     if (args[0] === "on") {
-      nickLockStatus[threadID] = true;
-      savedNicknames[threadID] = {};
+      global.nickLock[threadID] = true;
+      global.savedNick[threadID] = {};
 
       const info = await api.getThreadInfo(threadID);
-      for (const user of info.participantIDs) {
-        savedNicknames[threadID][user] =
-          info.nicknames[user] || "";
+      for (const uid of info.participantIDs) {
+        global.savedNick[threadID][uid] =
+          info.nicknames[uid] || "";
       }
 
-      return api.sendMessage("🔒 Nickname Lock ON", threadID);
-    }
-  },
-
-  onEvent: async function ({ api, event }) {
-    const threadID = event.threadID;
-
-    if (
-      nickLockStatus[threadID] &&
-      event.logMessageType === "log:user-nickname"
-    ) {
-      const userID = event.logMessageData.participant_id;
-      const originalNick = savedNicknames[threadID][userID] || "";
-
-      setTimeout(() => {
-        api.changeNickname(
-          originalNick,
-          threadID,
-          userID
-        );
-      }, 1000);
+      return api.sendMessage("🔒 Nickname lock ON", threadID);
     }
   }
 };
